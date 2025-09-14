4e1f4d0139d2bccdf0a4f5bd51631afa77c81973
caraido <38347809+caraido@users.noreply.github.com>
2025-09-14 04:48:02 +0000

    Deploying to gh-pages from @ caraido/caraido.github.io@597cb9f59db895d4d5698617005c04567e43d7ef 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "caraido/caraido",
+    "data-repo": "caraido/caraido.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
