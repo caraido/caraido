c4b6282bbdc03c5d57db1ed9d070137d4c94ce79
caraido <38347809+caraido@users.noreply.github.com>
2025-09-15 01:17:42 +0000

    Deploying to gh-pages from @ caraido/caraido.github.io@cac180be765cf74da445dbb1a196bbbbe23a4549 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,17 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-alec-presented-his-work-on-quot-aspects-of-speech-production-represented-in-inferior-frontal-gyrus-quot-at-the-2025-bci-meeting-in-banff-canada",
-          title: 'Alec presented his work on &amp;quot;Aspects of speech production represented in inferior frontal...',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/BCI_society_meeting_2025.html";
-            },},{id: "news-alec-received-nih-t32-training-grant-in-cognitive-neuroscience-support-for-his-phd-studies",
+        },{id: "news-alec-received-nih-t32-training-grant-in-cognitive-neuroscience-support-for-his-phd-studies",
           title: 'Alec received NIH T32 training grant in Cognitive Neuroscience support for his PhD...',
           description: "",
           section: "News",},{id: "news-alec-received-nih-t32-training-grant-in-cognitive-neuroscience-support-for-his-phd-studies",
@@ -51,6 +41,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/SNL_2024.html";
+            },},{id: "news-alec-presented-his-work-on-quot-aspects-of-speech-production-represented-in-inferior-frontal-gyrus-quot-at-the-2025-bci-meeting-in-banff-canada",
+          title: 'Alec presented his work on &amp;quot;Aspects of speech production represented in inferior frontal...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/BCI_society_meeting_2025.html";
             },},{
         id: 'social-email',
         title: 'email',
